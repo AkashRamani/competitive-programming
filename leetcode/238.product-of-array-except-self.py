06cc95d4f1f6575677fcd64801ce5e6b3dcd64e4
Akash <32708325+AkashRamani@users.noreply.github.com>
2024-11-08 07:40:58 -0600

    add: optimal solution

--- leetcode/238.product-of-array-except-self.py
+++ leetcode/238.product-of-array-except-self.py
@@ -8,6 +8,30 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         '''
+        Solution2 ==> Time O(n); Space O(1)
+
+        NOTE - Description says, the output array does not count as extra space for space complexity analysis
+        This is essentially Solution1 implemented with optimal use of memoery,
+
+        '''
+        out = [0 for i in range(0, len(nums))]
+
+        mul = 1
+        for i, num in enumerate(nums):
+            out[i] = mul
+            mul *= num
+        
+        mul = 1
+        for i in range(len(nums)-1, -1, -1):
+            out[i] = out[i] * mul
+            mul *=nums[i]
+
+        return out
+
+
+        '''
+        ###########################################################
+        Solution1 ==> Time O(n); Space O(n)
         idea/though process
             4,  5,  1,  2
         0   1      *    5*1*2
@@ -17,23 +41,25 @@ class Solution:
 
         '''
 
-        l = [0 for i in range(0, len(nums))] 
-        r = [0 for i in range(0, len(nums))]
+        # l = [0 for i in range(0, len(nums))] 
+        # r = [0 for i in range(0, len(nums))]
 
-        mul = 1
-        for i, num in enumerate(nums):
-            l[i] = mul
-            mul *= num 
+        # mul = 1
+        # for i, num in enumerate(nums):
+        #     l[i] = mul
+        #     mul *= num 
         
-        mul = 1
-        for i in range(len(nums)-1, -1, -1):
-            r[i] = mul
-            mul *=nums[i]
+        # mul = 1
+        # for i in range(len(nums)-1, -1, -1):
+        #     r[i] = mul
+        #     mul *=nums[i]
 
-        output = []
-        for i in range(0, len(nums)):
-            output.append(l[i]*r[i])
+        # output = []
+        # for i in range(0, len(nums)):
+        #     output.append(l[i]*r[i])
 
-        return output
+        # return output
+
+
 # @lc code=end
 

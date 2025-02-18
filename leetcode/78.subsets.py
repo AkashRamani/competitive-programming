53b96f734c0b624195b0f8cdfc1125ef6c9f523d
Akash <32708325+AkashRamani@users.noreply.github.com>
2025-02-17 23:22:58 -0600

    subsets

---
+++ leetcode/78.subsets.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=78 lang=python3
+#
+# [78] Subsets
+#
+
+# @lc code=start
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+            ans = set()
+        def back_track(subset, array):
+            ans.add(subset)
+            for i,elem in enumerate(array):
+                new_sub_set = subset+(elem,)
+                back_track(new_sub_set, array[i+1:])
+
+        back_track((), nums)
+        return [list(i) for i in list(ans)]
+
+        
+# @lc code=end
+

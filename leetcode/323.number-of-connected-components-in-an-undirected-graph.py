3db483bc771c77e520f6f52589fe45731c6fd809
Akash <32708325+AkashRamani@users.noreply.github.com>
2025-03-28 21:17:21 -0500

    no of connected components in a graph

---
+++ leetcode/323.number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,27 @@
+
+        '''
+            DFS.. but run an outer loop over all the node
+            everytime you call dfs from there are # of disconnected components
+        '''
+
+        adj = {i:[] for i in range(n)}
+        
+        for i,j in edges:
+            adj[i].append(j)
+            adj[j].append(i)
+
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for neighbor in adj[node]:
+                dfs(neighbor)
+            
+        count = 0
+        for node in range(n):
+            if node not in visited:
+                dfs(node)
+                count+=1
+        return count
+        
